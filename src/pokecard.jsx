fc2ea2720aec711afa5589d63bbf19ab67ae785d
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-28 19:29:06 -0700

    pokecard react object

---
+++ src/pokecard.jsx
@@ -0,0 +1,14 @@
+"use strict"
+
+/** PokeCard react object */
+
+const PokeCard = ({name, image, types, exp}) => {
+    (<div classname="PokeCard">
+        <span classname="PokeCard-name">{name}</span>
+        <img src={image} />
+        <span classname="PokeCard-type">Type: {types.join("/")}</span>
+        <span classname="PokeCard-exp">EXP: {exp}</span>
+    </div>)
+}
+
+export default PokeCard
